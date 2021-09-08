2625e60d60a2b112df46c7216220a1ced567e0ab
dj-mc <49037253+dj-mc@users.noreply.github.com>
2021-09-08 14:28:43 -0500

    Fix rotation bug, add shape transformations

--- rigid-bodies/rectangle.mjs
+++ rigid-bodies/rectangle.mjs
@@ -1,11 +1,15 @@
-import Vector2d from '../lib/vector2d.mjs';
 import RigidShape from './rigid-shape.mjs';
+import Vector2d from '../lib/vector2d.mjs';
 
 class Rectangle extends RigidShape {
-  constructor(center = new Vector2d(0, 0), width = 1, height = 1) {
+  constructor(center = new Vector2d(0, 0), width = 1, height = 1, fixed = true) {
     super(center);
+    this.shape = 'RigidShape :: Rectangle';
     this.width = width;
     this.height = height;
+    this.fixed = fixed;
+    this.vertices = [];
+    this.normals = [];
 
     //            norm[0]
     // vert[0] # ========= # vert[1]
@@ -13,34 +17,45 @@ class Rectangle extends RigidShape {
     // vert[3] # ========= # vert[2]
     //            norm[2]
 
-    this.vertices = [];
-
     this.vertices[0] = new Vector2d(center.x - width / 2, center.y - height / 2);
     this.vertices[1] = new Vector2d(center.x + width / 2, center.y - height / 2);
     this.vertices[2] = new Vector2d(center.x + width / 2, center.y + height / 2);
     this.vertices[3] = new Vector2d(center.x - width / 2, center.y + height / 2);
-
-    this.normals = [];
-
-    this.normals[0] = this.vertices[1].subtract(this.vertices[2]);
-    this.normals[0] = this.normals[0].normalized;
-
-    this.normals[1] = this.vertices[2].subtract(this.vertices[3]);
-    this.normals[1] = this.normals[1].normalized;
-
-    this.normals[2] = this.vertices[3].subtract(this.vertices[0]);
-    this.normals[2] = this.normals[2].normalized;
-
-    this.normals[3] = this.vertices[0].subtract(this.vertices[1]);
-    this.normals[3] = this.normals[3].normalized;
+    this.set_normals();
   }
 
-  draw_me(ctx) {
+  set_normals() {
+    for (let i = 0; i < 4; i++) {
+      let i_plus1 = i + 1 < 4 ? i + 1 : 0;
+      let i_plus2 = i + 2 < 4 ? i + 2 : 0;
+      this.normals[i] = this.vertices[i_plus1].subtract(this.vertices[i_plus2]);
+      this.normals[i] = this.normals[i].normalized;
+    }
+  }
+
+  draw_shape(ctx) {
     ctx.save();
     ctx.translate(this.vertices[0].x, this.vertices[0].y);
     ctx.rotate(this.angle);
     ctx.strokeRect(0, 0, this.width, this.height);
     ctx.restore();
+  }
+
+  rotate_shape(angle) {
+    this.angle += angle;
+    for (let i = 0; i < this.vertices.length; i++) {
+      this.vertices[i] = this.vertices[i].rotate(this.center, angle);
+    }
+    this.set_normals();
+    return this;
+  }
+
+  move_towards(vect) {
+    for (let i = 0; i < this.vertices.length; i++) {
+      this.vertices[i] = this.vertices[i].add(vect);
+    }
+    this.center = this.center.add(vect);
+    return this;
   }
 }
 
