f439753c6b7b0f496cbaadaed5b2745a2fb287c3
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-10-05 02:19:20 -0500

    Draw collision info of two overlapping circles

--- engine-core/physics.mjs
+++ engine-core/physics.mjs
@@ -1,16 +1,43 @@
 import CurrentState from './state.mjs';
+import CollisionData from '../lib/collision.mjs';
 
 class Physics {
   constructor() {}
 
+  static draw_collision_info(collision_info, context) {
+    context.beginPath();
+    context.moveTo(collision_info.start.x, collision_info.start.y);
+    context.lineTo(collision_info.end.x, collision_info.end.y);
+    context.closePath();
+    context.strokeStyle = 'Orange';
+    context.stroke();
+  }
+
   static collision() {
     // 5 = borders + cursor
+    const collision_data = new CollisionData();
     for (let i = 5; i < CurrentState.objs_len; i++) {
       for (let j = i + 1; j < CurrentState.objs_len; j++) {
+        // Check bound and collision tests
         if (CurrentState.collection[i].bound_test(CurrentState.collection[j])) {
-          CurrentState.context.strokeStyle = 'lightblue';
-          CurrentState.collection[i].draw_shape(CurrentState.context);
-          CurrentState.collection[j].draw_shape(CurrentState.context);
+          if (
+            CurrentState.collection[i].collision_test(CurrentState.collection[j], collision_data)
+          ) {
+            if (
+              // Validate a collision: ensure that its normal vector is pointing
+              // towards the opposing object being tested. Use the sign of the
+              // dot product between the collision's normal vector and the
+              // difference vector of their centers.
+              collision_data
+                .get_normal_vector()
+                .dot_product(
+                  CurrentState.collection[j].center.subtract(CurrentState.collection[i].center)
+                ) < 0
+            ) {
+              collision_data.change_direction();
+            }
+            this.draw_collision_info(collision_data, CurrentState.context);
+          }
         }
       }
     }
--- rigid-bodies/circle.mjs
+++ rigid-bodies/circle.mjs
@@ -32,6 +32,58 @@ class Circle extends RigidShape {
     this.center = this.center.add(vect);
     return this;
   }
+
+  collided_circle_circle(c1, c2, collision_info) {
+    const vector_c1_c2 = c2.center.subtract(c1.center);
+    const radius_sum = c1.radius + c2.radius;
+    const distance = vector_c1_c2.v_length;
+
+    if (distance > Math.sqrt(radius_sum ** 2)) {
+      return false; // No overlap found
+    }
+
+    if (distance !== 0) {
+      // Possible overlap found
+      const normalized_c2_c1 = vector_c1_c2.scalar(-1).normalized;
+      const radius_c2 = normalized_c2_c1.scalar(c2.radius);
+
+      collision_info.set_info(
+        radius_sum - distance,
+        vector_c1_c2.normalized,
+        c2.center.add(radius_c2)
+      );
+    } else {
+      // Both circles' centers occupy the same position
+      if (c1.radius > c2.radius) {
+        // c1 is larger than c2
+        collision_info.set_info(
+          radius_sum,
+          new Vector2d(0, -1),
+          c1.center.add(new Vector2d(0, c1.radius))
+        );
+      } else {
+        // c2 is bigger than c1
+        collision_info.set_info(
+          radius_sum,
+          new Vector2d(0, -1),
+          c2.center.add(new Vector2d(0, c2.radius))
+        );
+      }
+    }
+    return true;
+  }
+
+  collision_test(other_shape, collision_info) {
+    let status = false;
+    if (other_shape.shape === 'RigidShape :: Circle') {
+      // Two colliding circles
+      status = this.collided_circle_circle(this, other_shape, collision_info);
+    } else {
+      // Not two colliding circles
+      status = false;
+    }
+    return status;
+  }
 }
 
 export default Circle;
